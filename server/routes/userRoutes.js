fd2a5fdb00acaa5392e33bfe3ac6cb3807ff15bd
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 14:46:45 +0500

    Create userRoutes.js

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const registerController = require("../controllers/userContoller");
+
+const router = express.Router();
+
+router.post("/register",registerController)
+
+module.exports = router;
