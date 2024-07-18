78b644650e20de2e6d7beece9e0b250904369356
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 14:46:48 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,20 +3,19 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDataBase = require("./config/DataBase");
 
 dotenv.config();
+
+connectDataBase();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to the E-commerce Store API!",
-  });
-});
+app.use("/api/v1/auth/", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 3000;
 
