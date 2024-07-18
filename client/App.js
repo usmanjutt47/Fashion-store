0ce86fffb84c3bf619e0273cb160570e4851779c
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 15:00:59 +0500

    Update App.js

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/auth/Login";
+import SignUp from "./src/auth/SignUp";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,11 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
