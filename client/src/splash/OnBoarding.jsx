4858e9ff9c99920d60e87331cfc00f2a94be59df
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-19 13:17:33 +0500

    updated

--- client/src/splash/OnBoarding.jsx
+++ client/src/splash/OnBoarding.jsx
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { useFonts } from "expo-font";
 
 export default function OnBoarding() {
+  const [loaded] = useFonts({
+    GolosText: require("../../assets/fonts/GolosText[wght].ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>OnBoarding</Text>
+    <View style={{ backgroundColor: "red" }}>
+      <View style={{ position: "relative" }}>
+        <Image
+          source={require("../../assets/splashAssets/splash.png")} // replace with your image path
+          style={{ width: 100, height: 100 }} // set the desired width and height
+        />
+      </View>
+      <Text style={{ fontFamily: "GolosText", fontSize: 40 }}>OnBoarding</Text>
     </View>
   );
 }
