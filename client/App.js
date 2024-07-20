72c55d873c6d1111fcb76e02b264a4bf5acb4038
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-20 13:00:38 +0500

    Update App.js

--- client/App.js
+++ client/App.js
@@ -4,42 +4,31 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/auth/Login";
 import SignUp from "./src/auth/SignUp";
 import OnBoarding from "./src/splash/OnBoarding";
-import { useFonts } from "expo-font";
 
 const Stack = createStackNavigator();
 
 export const FontContext = createContext();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    GolosText: require("./assets/fonts/GolosText[wght].ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null; // Show a loading spinner or some fallback UI here
-  }
-
   return (
-    <FontContext.Provider value={{ fontFamily: "GolosText" }}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="OnBoarding">
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="OnBoarding"
-            component={OnBoarding}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </FontContext.Provider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="OnBoarding">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="OnBoarding"
+          component={OnBoarding}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
