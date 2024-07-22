96a40619324628ac343dd08467bff213a6308e84
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-22 12:24:48 +0500

    Update SignUp.jsx

--- client/src/auth/SignUp.jsx
+++ client/src/auth/SignUp.jsx
@@ -6,12 +6,13 @@ import {
   StyleSheet,
   Pressable,
   TextInput,
+  Image,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { BlurView } from "expo-blur";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
-import CountryPicker from "react-native-country-picker-modal"; // Import the country picker
+import CountryPicker from "react-native-country-picker-modal";
 
 export default function SignUp() {
   const navigation = useNavigation();
@@ -19,115 +20,79 @@ export default function SignUp() {
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
-  const [country, setCountry] = useState(null); // State to hold the selected country
+  const [country, setCountry] = useState(null);
   const [isNameFocused, setIsNameFocused] = useState(false);
   const [isEmailFocused, setIsEmailFocused] = useState(false);
   const [isPhoneNumberFocused, setIsPhoneNumberFocused] = useState(false);
   const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-  const [showCountryPicker, setShowCountryPicker] = useState(false); // State to toggle country picker
+  const [showCountryPicker, setShowCountryPicker] = useState(false);
+
+  const [countryCode, setCountryCode] = useState("US");
+  const [withCountryNameButton, setWithCountryNameButton] = useState(false); // Hide country name
+  const [withFlag, setWithFlag] = useState(true);
+  const [withEmoji, setWithEmoji] = useState(false);
+  const [withFilter, setWithFilter] = useState(true);
+  const [withAlphaFilter, setWithAlphaFilter] = useState(false);
+  const [withCallingCode, setWithCallingCode] = useState(true);
 
   const handleSelectCountry = (country) => {
     setCountry(country);
-    setShowCountryPicker(false); // Hide the picker after selection
+    setCountryCode(country.cca2);
+    setShowCountryPicker(false);
+  };
+
+  // Method to get the flag image URL
+  const getFlagImageUrl = (countryCode) => {
+    return `https://www.countryflags.io/${countryCode}/shiny/64.png`;
   };
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.imageContainer}>
-        {/* Background Image */}
         <ImageBackground
           source={require("../../assets/splashAssets/splash.png")}
           style={styles.image}
           blurRadius={2.5}
         >
-          <View
-            style={{
-              height: 100,
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 20,
-            }}
-          >
-            {/* Back button */}
-            <BlurView
-              style={{
-                width: 44,
-                height: 44,
-                justifyContent: "center",
-                borderRadius: 36,
-                marginLeft: "5%",
-                overflow: "hidden",
-              }}
-            >
+          <View style={styles.headerContainer}>
+            <BlurView style={styles.backButton}>
               <Pressable onPress={() => navigation.navigate("Login")}>
-                <Ionicons
-                  name="chevron-back"
-                  size={24}
-                  color="#1E1E1E"
-                  style={{ alignSelf: "center" }}
-                />
+                <Ionicons name="chevron-back" size={24} color="#1E1E1E" />
               </Pressable>
             </BlurView>
-
-            {/* Elegancia heading */}
-            <View
-              style={{
-                flex: 1,
-                position: "absolute",
-                width: "100%",
-              }}
-            >
-              <Text style={[styles.eleganciaHeading]}>Elegancia</Text>
+            <View style={styles.headingContainer}>
+              <Text style={styles.eleganciaHeading}>Elegancia</Text>
             </View>
           </View>
 
-          {/* SignUp Container */}
           <BlurView
             style={styles.blurViewContainer}
             intensity={100}
             tint="default"
             experimentalBlurMethod="dimezisBlurView"
           >
-            <Text style={[styles.createAccountHeading]}>Create Account</Text>
-
-            {/* SignUp form section */}
+            <Text style={styles.createAccountHeading}>Create Account</Text>
             <View style={styles.signUpFormSection}>
-              {/* Name label and input field */}
               <Text style={styles.label}>Name</Text>
               <TextInput
-                style={[
-                  styles.nameInput,
-                  isNameFocused && {
-                    borderColor: "#fff",
-                    backgroundColor: "#fff",
-                  },
-                ]}
+                style={[styles.nameInput, isNameFocused && styles.focusedInput]}
                 placeholder="Enter Your Name"
                 placeholderTextColor="#4e4e4e"
-                selectionColor={"#000"}
-                cursorColor={"#4e4e4e"}
                 value={name}
                 onChangeText={setName}
                 onFocus={() => setIsNameFocused(true)}
                 onBlur={() => setIsNameFocused(false)}
-                keyboardType="name-phone-pad"
               />
 
-              {/* Email label and input field */}
               <Text style={[styles.label, styles.marginTop]}>Email</Text>
               <TextInput
                 style={[
                   styles.emailInput,
-                  isEmailFocused && {
-                    borderColor: "#fff",
-                    backgroundColor: "#fff",
-                  },
+                  isEmailFocused && styles.focusedInput,
                 ]}
                 placeholder="Enter Your Email"
                 placeholderTextColor="#4e4e4e"
-                selectionColor={"#000"}
-                cursorColor={"#4e4e4e"}
                 value={email}
                 onChangeText={setEmail}
                 onFocus={() => setIsEmailFocused(true)}
@@ -135,53 +100,46 @@ export default function SignUp() {
                 keyboardType="email-address"
               />
 
-              {/* Phone label and input field */}
               <Text style={styles.label}>Phone number</Text>
-              <View style={styles.phoneInputContainer}>
-                <Pressable
-                  style={styles.countryPickerButton}
-                  onPress={() => setShowCountryPicker(true)}
-                >
-                  <Text style={styles.countryText}>
-                    {country ? `+${country.callingCode[0]}` : "+00"}
-                  </Text>
-
-                  <Ionicons name="chevron-down" size={24} color="#1E1E1E" />
+              <View style={styles.phoneInputWrapper}>
+                <Pressable onPress={() => setShowCountryPicker(true)}>
+                  <CountryPicker
+                    containerButtonStyle={{ marginLeft: 15 }}
+                    countryCode={countryCode}
+                    withFlag={withFlag}
+                    withCallingCodeButton={true}
+                    withEmoji={withEmoji}
+                    withFilter={withFilter}
+                    withAlphaFilter={withAlphaFilter}
+                    withCallingCode={withCallingCode}
+                    onSelect={handleSelectCountry}
+                    visible={showCountryPicker}
+                    onClose={() => setShowCountryPicker(false)}
+                  />
                 </Pressable>
+
                 <TextInput
                   style={[
                     styles.phoneNumberInput,
-                    isPhoneNumberFocused && {
-                      borderColor: "#fff",
-                      backgroundColor: "#fff",
-                    },
+                    isPhoneNumberFocused && styles.focusedInput,
                   ]}
                   placeholder="Enter Your Phone Number"
                   placeholderTextColor="#4e4e4e"
-                  selectionColor={"#000"}
-                  cursorColor={"#4e4e4e"}
                   value={phoneNumber}
                   onChangeText={setPhoneNumber}
                   onFocus={() => setIsPhoneNumberFocused(true)}
                   onBlur={() => setIsPhoneNumberFocused(false)}
-                  keyboardType="name-phone-pad"
                 />
               </View>
 
-              {/* Password label and input field */}
               <Text style={[styles.label, styles.marginTop]}>Password</Text>
               <TextInput
                 style={[
                   styles.passwordInput,
-                  isPasswordFocused && {
-                    borderColor: "#fff",
-                    backgroundColor: "#fff",
-                  },
+                  isPasswordFocused && styles.focusedInput,
                 ]}
                 placeholder="Enter Password"
                 placeholderTextColor="#4e4e4e"
-                selectionColor={"#000"}
-                cursorColor={"#004e4e4e"}
                 value={password}
                 onChangeText={setPassword}
                 onFocus={() => setIsPasswordFocused(true)}
@@ -189,51 +147,25 @@ export default function SignUp() {
                 secureTextEntry
               />
             </View>
+            <StatusBar style="auto" />
 
-            <View
-              style={{
-                width: "80%",
-                alignSelf: "center",
-                marginTop: "46%",
-              }}
-            >
+            <View style={styles.footerContainer}>
               <Pressable
                 onPress={() => navigation.navigate("SignUp")}
-                style={{
-                  flexDirection: "row",
-                  marginBottom: "2%",
-                }}
+                style={styles.loginLink}
               >
-                <Text style={{ fontWeight: "medium", color: "#DCDCDB" }}>
+                <Text style={styles.loginText}>
                   I have an account?{" "}
+                  <Text style={styles.loginLinkText}>Login</Text>
                 </Text>
-                <Pressable onPress={() => navigation.navigate("SignUp")}>
-                  <Text style={{ fontWeight: "bold", color: "#fff" }}>
-                    Login
-                  </Text>
-                </Pressable>
               </Pressable>
-              <Pressable
-                style={{
-                  backgroundColor: "#3AA2ED",
-                  height: 48,
-                  justifyContent: "center",
-                  borderRadius: 33,
-                }}
-                // onPress={handleLogin}
-              >
-                <Text style={[styles.buttonText]}>Sign up</Text>
+              <Pressable style={styles.signUpButton}>
+                <Text style={styles.buttonText}>Sign up</Text>
               </Pressable>
             </View>
           </BlurView>
         </ImageBackground>
       </View>
-      <CountryPicker
-        visible={showCountryPicker}
-        withCallingCode
-        onSelect={handleSelectCountry}
-        onClose={() => setShowCountryPicker(false)}
-      />
     </View>
   );
 }
@@ -250,6 +182,25 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
   },
+  headerContainer: {
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  backButton: {
+    width: 44,
+    height: 44,
+    justifyContent: "center",
+    borderRadius: 36,
+    marginLeft: "5%",
+    overflow: "hidden",
+  },
+  headingContainer: {
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+  },
   eleganciaHeading: {
     fontWeight: "bold",
     fontSize: 30,
@@ -264,12 +215,6 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 50,
     borderTopLeftRadius: 50,
     overflow: "hidden",
-  },
-  welcomeHeading: {
-    fontSize: 30,
-    color: "#1E1E1E",
-    alignSelf: "center",
-    marginTop: 20,
   },
   createAccountHeading: {
     fontSize: 30,
@@ -313,32 +258,33 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     color: "#1E1E1E",
   },
-  phoneInputContainer: {
+  phoneInputWrapper: {
     flexDirection: "row",
     alignItems: "center",
-    height: 48,
     borderColor: "#6F7072",
     borderWidth: 1,
     borderRadius: 30,
-    // paddingLeft: 20,
+    marginTop: 10,
     backgroundColor: "transparent",
   },
-  countryPickerButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginRight: 10,
-    overflow: "hidden",
+  flagImage: {
+    width: 48,
+    height: 48,
+    marginLeft: 10,
   },
-  countryText: {
-    color: "#1E1E1E",
+  countryCodeText: {
     fontWeight: "bold",
+    color: "#1E1E1E",
+    marginLeft: 10,
+    marginRight: 5,
   },
   phoneNumberInput: {
     flex: 1,
-    fontWeight: "bold",
+    paddingLeft: 10,
+    paddingRight: 20,
+    height: 48,
+    backgroundColor: "transparent",
     color: "#1E1E1E",
-    height: "100%",
-    width: "100%",
   },
   passwordInput: {
     fontWeight: "bold",
@@ -351,20 +297,33 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     color: "#1E1E1E",
   },
-  button: {
-    backgroundColor: "#3AA2ED",
-    height: 48,
-    justifyContent: "center",
-    width: "85%",
-    borderRadius: 36,
-    alignSelf: "center",
-    marginTop: "47%",
+  focusedInput: {
+    borderColor: "#007bff",
+  },
+  footerContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  loginLink: {
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "#1E1E1E",
+    fontSize: 16,
+  },
+  loginLinkText: {
+    fontWeight: "bold",
+    color: "#007bff",
+  },
+  signUpButton: {
+    backgroundColor: "#007bff",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 30,
   },
   buttonText: {
-    fontWeight: "medium",
-    textAlign: "center",
-    color: "#fff",
+    color: "#ffffff",
     fontSize: 16,
-    fontFamily: "GolosText",
+    fontWeight: "bold",
   },
 });
