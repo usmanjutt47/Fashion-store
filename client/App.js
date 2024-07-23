e5ba9398ffcfb013a6afafc27a13dd9b714c2dea
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-23 15:04:16 +0500

    Update App.js

--- client/App.js
+++ client/App.js
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="OnBoarding"
+        initialRouteName="Home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={Login} />
