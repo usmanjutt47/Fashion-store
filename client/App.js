12b7158de4946a86839f19dbbb6a8825e8b51d6c
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-07-26 15:53:10 +0500

    Bottom Tabs Navigator Created

--- client/App.js
+++ client/App.js
@@ -1,27 +1,90 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Image } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons"; 
 import Login from "./src/auth/Login";
 import SignUp from "./src/auth/SignUp";
 import OnBoarding from "./src/splash/OnBoarding";
 import Home from "./src/screens/Home";
 import ForgotPassword from "./src/screens/ForgotPassword";
+import Cart from "./src/TabsScreens/Cart";
+import Profile from "./src/TabsScreens/Profile";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+    </Stack.Navigator>
+  );
+}
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="OnBoarding"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="OnBoarding" component={OnBoarding} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconComponent;
+
+            if (route.name === "HomeStack") {
+              iconComponent = (
+                <Image
+                  source={require("./assets/icon/home.png")}
+                  style={{ width: size, height: size, tintColor: color }}
+                />
+              );
+            } else if (route.name === "Cart") {
+              iconComponent = (
+                <Image
+                  source={require("./assets/icon/cart.png")}
+                  style={{ width: size, height: size, tintColor: color }}
+                />
+              );
+            } else if (route.name === "Profile") {
+              iconComponent = (
+                <FontAwesome5 name="user-alt" size={size} color={color} />
+              );
+            }
+
+            return iconComponent;
+          },
+          tabBarActiveTintColor: "#3AA2ED",
+          tabBarInactiveTintColor: "#B2BAC2",
+          tabBarStyle: {
+            backgroundColor: "transparent", 
+            borderTopWidth: 0, 
+            elevation: 0,
+          },
+          tabBarLabel: () => null,
+        })}
       >
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-      </Stack.Navigator>
+        <Tab.Screen name="HomeStack" component={HomeStack} />
+        <Tab.Screen name="Cart" component={Cart} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
