0f4ee8053b571ab90a91d416ba7b4cc7efe30d43
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-30 16:08:24 +0500

    Update Home.jsx

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -23,6 +23,51 @@ const buttonData = [
   { key: "Pants", label: "Pants", width: 101 },
 ];
 
+const cardData = [
+  {
+    id: "1",
+    image: require("../../assets/images/category1.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+  {
+    id: "2",
+    image: require("../../assets/images/category2.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+  {
+    id: "3",
+    image: require("../../assets/images/category3.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+  {
+    id: "4",
+    image: require("../../assets/images/category4.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+  {
+    id: "5",
+    image: require("../../assets/images/category5.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+  {
+    id: "6",
+    image: require("../../assets/images/Card.png"),
+    title: "Flex Shirt Cream",
+    type: "T-shirt",
+    price: "$299",
+  },
+];
+
 export default function Home() {
   const [loaded] = useFonts({
     GolosText: require("../../assets/fonts/GolosText[wght].ttf"),
@@ -38,32 +83,43 @@ export default function Home() {
     setSelected(item);
   };
 
-  const data = ["All", "Hoodie", "Sweater", "T-Shirt", "Pants"];
-
-  // Render function for FlatList
   const renderItem = ({ item }) => (
-    <TouchableOpacity
-      onPress={() => handlePress(item)}
-      style={[
-        styles.pressableButton,
-        {
-          backgroundColor: selected === item ? "#000000" : "#EBF2F4",
-          width: item === "All" ? 66 : 94,
-          marginHorizontal: 5,
-        },
-      ]}
-    >
-      <Text
-        style={[
-          styles.pressableText,
-          {
-            color: selected === item ? "#FFFFFF" : "#000000",
-          },
-        ]}
-      >
-        {item}
-      </Text>
-    </TouchableOpacity>
+    <View style={styles.card}>
+      <Pressable style={styles.itemContainer}>
+        <Image source={item.image} style={styles.itemImage} />
+        <View style={styles.itemTextContainer}>
+          <Text style={styles.itemText}>{item.title}</Text>
+          <Text style={{ color: "#b5c5c7" }}>{item.type}</Text>
+        </View>
+        <View style={styles.itemBlurContainer}>
+          <BlurView
+            style={styles.blurContainer}
+            intensity={50}
+            tint="default"
+            experimentalBlurMethod="dimezisBlurView"
+          >
+            <Pressable>
+              <Text style={styles.priceText}>{item.price}</Text>
+            </Pressable>
+          </BlurView>
+          <BlurView
+            style={styles.blurView}
+            intensity={50}
+            tint="default"
+            experimentalBlurMethod="dimezisBlurView"
+          >
+            <Pressable>
+              <AntDesign
+                name="heart"
+                color="#fff"
+                size={20}
+                style={{ alignSelf: "center" }}
+              />
+            </Pressable>
+          </BlurView>
+        </View>
+      </Pressable>
+    </View>
   );
 
   return (
@@ -91,254 +147,50 @@ export default function Home() {
       >
         <Text style={styles.collectionTitle}>Collection</Text>
         <FlatList
-          data={data}
-          renderItem={renderItem}
+          data={buttonData.map((item) => item.label)}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => handlePress(item)}
+              style={[
+                styles.pressableButton,
+                {
+                  backgroundColor: selected === item ? "#000000" : "#EBF2F4",
+                  width: item === "All" ? 66 : 94,
+                  marginHorizontal: 5,
+                },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.pressableText,
+                  { color: selected === item ? "#FFFFFF" : "#000000" },
+                ]}
+              >
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
           keyExtractor={(item) => item}
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.buttonContainer}
         />
-
         <View style={styles.carouselContainer}>
           <Carousel />
         </View>
 
         <View style={styles.scrollInnerContainer}>
           <Text style={styles.tShirtText}>Elegance T-shirt</Text>
-          <View style={[styles.imageContainer]}>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/category1.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={{
-                    width: 41,
-                    height: 41,
-                    justifyContent: "center",
-                    borderRadius: 36,
-                    overflow: "hidden",
-                  }}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/category2.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={styles.blurView}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-          </View>
-          <View style={styles.imageContainer}>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/category3.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={styles.blurView}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/category4.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={styles.blurView}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-          </View>
-          <View style={[styles.imageContainer, { marginBottom: "5%" }]}>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/category5.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={styles.blurView}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-            <Pressable style={styles.itemContainer}>
-              <Image
-                source={require("../../assets/images/Card.png")}
-                style={styles.itemImage}
-              />
-              <View style={styles.itemTextContainer}>
-                <Text style={styles.itemText}>Flex Shirt Cream</Text>
-                <Text style={{ color: "#b5c5c7" }}>T-shirt</Text>
-              </View>
-              <View style={styles.itemBlurContainer}>
-                <BlurView
-                  style={styles.blurContainer}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <Text style={styles.priceText}>$299</Text>
-                  </Pressable>
-                </BlurView>
-                <BlurView
-                  style={styles.blurView}
-                  intensity={50}
-                  tint="default"
-                  experimentalBlurMethod="dimezisBlurView"
-                >
-                  <Pressable>
-                    <AntDesign
-                      name="heart"
-                      color="#fff"
-                      size={20}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </Pressable>
-                </BlurView>
-              </View>
-            </Pressable>
-          </View>
+          <FlatList
+            data={cardData}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            columnWrapperStyle={styles.columnWrapper}
+            contentContainerStyle={styles.cardContainer}
+            showsVerticalScrollIndicator={false}
+          />
+          <Text style={styles.collectionTitle}>Collection</Text>
           <View style={[styles.carouselContainer, { marginBottom: "5%" }]}>
             <Carousel />
           </View>
@@ -426,21 +278,19 @@ const styles = StyleSheet.create({
     fontFamily: "GolosText",
     fontSize: 24,
   },
-  imageContainer: {
-    flexDirection: "row",
-    width: "100%",
-    height: 264,
-    gap: 10,
-    marginTop: "5%",
+  cardContainer: {
+    paddingBottom: 20,
   },
-  imagePressable: {
-    backgroundColor: "red",
-    height: "100%",
-    width: "50%",
+  columnWrapper: {
+    justifyContent: "space-between",
+    marginBottom: 15,
+  },
+  card: {
+    width: "48.5%",
   },
   itemContainer: {
-    width: "48.5%",
-    height: "100%",
+    width: "100%",
+    height: 264,
     borderRadius: 30,
     overflow: "hidden",
   },
@@ -480,15 +330,14 @@ const styles = StyleSheet.create({
   },
   priceText: {
     alignSelf: "center",
-    letterSpacing: 0.4,
-    fontSize: 16,
-    fontWeight: "bold",
+    letterSpacing: 0.5,
     fontFamily: "GolosText",
+    fontSize: 15,
     color: "#fff",
   },
   blurView: {
-    width: 41,
-    height: 41,
+    width: 44,
+    height: 44,
     justifyContent: "center",
     borderRadius: 36,
     overflow: "hidden",
