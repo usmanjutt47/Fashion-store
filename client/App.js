71c92cf3ec05ce14b928da36f8959eb49bc02016
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-01 14:26:34 +0500

    Update App.js

--- client/App.js
+++ client/App.js
@@ -19,7 +19,10 @@ const Tab = createBottomTabNavigator();
 
 function MainStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="HomeStack"
+    >
       <Stack.Screen name="Auth" component={AuthOrMainScreen} />
       <Stack.Screen name="HomeStack" component={HomeStack} />
       <Stack.Screen name="ProductDetails" component={ProductDetail} />
